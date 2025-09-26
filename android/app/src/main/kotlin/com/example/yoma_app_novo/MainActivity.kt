cccdd621b9ade2c2f1e1ee050216f540aece392f
Patricia Giorgetto <patriciagiorgetto@hotmail.com>
2025-09-26 18:35:46 -0300

    Migração para Flutter embedding v2 e ajustes no build (commit limpo)

--- android/app/src/main/kotlin/com/example/yoma_app_novo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yoma_app_novo/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.yoma_app_novo
+package com.seu.pacote
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
