fde4bc06fbf6ec17e73122b58d82393eed42283d
Patricia Giorgetto <patriciagiorgetto@hotmail.com>
2025-09-26 22:02:48 -0300

    Ajusta MainActivity.kt para o pacote correto

--- android/app/src/main/kotlin/com/example/yoma_app_novo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yoma_app_novo/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.yoma_app_novo
+package com.patriciagiorgetto.yoma
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
