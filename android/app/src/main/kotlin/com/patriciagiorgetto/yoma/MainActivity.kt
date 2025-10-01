3736cc88a9f53de017977d071523a0a897a4f9f2
Patricia Giorgetto <patriciagiorgetto@hotmail.com>
2025-10-01 18:18:43 -0300

    Corrige AndroidManifest.xml válido para embedding v2

---
+++ android/app/src/main/kotlin/com/patriciagiorgetto/yoma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.patriciagiorgetto.yoma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,63 +1,8 @@
- <HEAD>
-// build.gradle.kts (nível do projeto Android)
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-
- <HEAD>
-import java.util.Properties
-import java.io.FileInputStream
-
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
-android {
-    namespace = "com.patriciagiorgetto.yoma"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
- 26db685 (Atualiza .gitignore e remove rastreamento de arquivos de build antigos)
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-flutter {
-    source = "../.."
-
-// build.gradle.kts (nível do projeto Android)
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
- 26db685 (Atualiza .gitignore e remove rastreamento de arquivos de build antigos)
 }
 
 val newBuildDir: Directory =
